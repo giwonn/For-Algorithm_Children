38c207e21f0960c39cf67c542ad6690bcde7b4c0
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-27 20:52:28 +0900

    phone
    substring 사용할것

---
+++ Taerin/Algorithm/src/week1/phone.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class phone {
+
+	public static void main(String[] args) {
+		//핸드폰 뒷자리 4개를 제외하고 전부 ****처리
+		//phone_number 문자열
+		//substring 사용
+		//substrung-4 
+		
+		
+		
+		
+
+	}
+
+}
