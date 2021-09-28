3a94818d890cdedf000338a819a1ea7759338112
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-28 23:34:43 +0900

    답1

--- Taerin/Algorithm/src/week1/phone.java
+++ Taerin/Algorithm/src/week1/phone.java
@@ -8,7 +8,20 @@ public class phone {
 		//substring 사용
 		//substrung-4 
 		
-		
+		String s = "";
+//		class Solution {
+//		    public String solution(String phone_number) {		 
+//			StringBuilder answer = new StringBuilder(phone_number);
+//				 
+//			for(int i = 0; i < phone_number.length()-4; i++) {
+//				answer.setCharAt(i, '*');
+//			}
+//			return answer.toString();
+//		    }
+//		}
+			
+
+		출처: https://ljg960730.tistory.com/20 [MINIMALISM_JK]
 		
 		
 
