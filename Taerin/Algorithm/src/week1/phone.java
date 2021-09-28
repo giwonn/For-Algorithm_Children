7b5c9f508005265c9a484b5ed01f55e7935f704b
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-28 23:38:05 +0900

    답2
    substring 사용할것 그리고 마지막 4자리를 제외하고 **** 표시로 개인정보를 가릴것

--- Taerin/Algorithm/src/week1/phone.java
+++ Taerin/Algorithm/src/week1/phone.java
@@ -3,11 +3,11 @@ package week1;
 public class phone {
 
 	public static void main(String[] args) {
-		//핸드폰 뒷자리 4개를 제외하고 전부 ****처리
-		//phone_number 문자열
-		//substring 사용
-		//substrung-4 
-		
+		// 핸드폰 뒷자리 4개를 제외하고 전부 ****처리
+		// phone_number 문자열
+		// substring 사용
+		// substrung-4
+
 		String s = "";
 //		class Solution {
 //		    public String solution(String phone_number) {		 
@@ -19,12 +19,22 @@ public class phone {
 //			return answer.toString();
 //		    }
 //		}
-			
+//	__________________________________________________ 답 1
 
-		출처: https://ljg960730.tistory.com/20 [MINIMALISM_JK]
-		
-		
-
+//		class Solution {
+//			public String solution(String phone_number) {
+//				String answer = "";
+//				for (int i = 0; i < phone_number.length(); i++) {
+//					if (i < phone_number.length() - 4) {
+//						answer += "*";
+//					} else {
+//						answer += phone_number.charAt(i);
+//					}
+//				}
+//				return answer;
+//			}
+//		}
+//   ____________________________________________________ 답 2
 	}
 
 }
