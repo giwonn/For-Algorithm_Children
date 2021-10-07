95ccd433b0ece4d0d4b59dfe6b5bf34f4a729595
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-07 17:00:47 +0900

    Update remove_duplication.java

--- Taerin/Algorithm/src/week2/remove_duplication.java
+++ Taerin/Algorithm/src/week2/remove_duplication.java
@@ -1,11 +1,29 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class remove_duplication {
 
 	public static void main(String[] args) {
 		// 중복값 제거하기
+		int[] answer = {};
 		
+		solution(answer);
 
+	}
+	private static int solution(int []arr){
+		//arraylist or linkedlist
+		 
+		//<> 제네릭임 그래서 int 사용 불가 무조건 integer사용 
+		//integer는 래퍼클래스라서 
+		
+		
+		//배열로 변환하는 함수 사용할것
+		ArrayList<Integer> answer = new ArrayList<>();
+		
+		
+		return answer;
 	}
 
 }
