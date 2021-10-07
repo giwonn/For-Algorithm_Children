0fba890acfefea6286fdc94c29227e4d4d3ef440
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-07 14:50:51 +0900

    Update phone2.java

--- Taerin/Algorithm/src/week1/phone2.java
+++ Taerin/Algorithm/src/week1/phone2.java
@@ -3,8 +3,27 @@ package week1;
 public class phone2 {
 
 	public static void main(String[] args) {
-		// substring 사용
-
+		
+		System.out.println(solution("01022223333"));
+	}
+	
+	private static String solution(String phone_number) {
+		// substring 사용하기
+		//1. 번호 길이구하기
+		//2. 마지막 4자리 제외한 *로 바꾸기
+		//3. 출력
+		
+		String answer = "";
+		//length와 lenght()차이 스트링에서만 오직 length();
+		
+		for(int i = 0; i < phone_number.length()-4; i++) {
+				answer += "*";
+		}
+		
+		// substring(start, end) => start부터 end-1까지 리턴 (end는 생략가능)
+		answer += phone_number.substring(phone_number.length()-4);
+		
+		return answer;
 	}
 
 }
